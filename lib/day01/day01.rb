ce6b06db82ee8799f36aed4200d9df2f3c5fa136
Stefan Filev <sfilev@expediagroup.com>
2022-12-01 18:43:43 +0000

    solution for day01 and test for it

--- lib/day01/day01.rb
+++ lib/day01/day01.rb
@@ -1,1 +1,35 @@
-require 'pry-byebug'
+def calculate(food)
+  current_best = 0
+  current_value = 0
+  arr = food.split("\n")
+  arr << ''
+  arr.each do |element|
+    if element == ''
+      current_best = current_value > current_best ? current_value : current_best
+      current_value = 0
+    end
+    current_value += element.to_i
+  end
+  current_best
+end
+
+def calculate_top_three(food)
+  current_value = 0
+  arr = food.split("\n")
+  arr << ''
+  arr_sum = []
+  arr.each do |element|
+    if element == ''
+      arr_sum << current_value
+      current_value = 0
+    else
+      current_value += element.to_i
+    end
+  end
+  high = arr_sum.max
+  arr_sum.delete_at(arr_sum.index(high))
+  mid = arr_sum.max
+  arr_sum.delete_at(arr_sum.index(mid))
+  low = arr_sum.max
+  high + mid + low
+end
--- spec/day01/day01_spec.rb
+++ spec/day01/day01_spec.rb
@@ -1,1 +1,27 @@
 require_relative '../../lib/day01/day01'
+RSpec.describe 'Finding elf with most food' do
+  let(:food) do
+    '1000
+    2000
+    3000
+
+    4000
+
+    5000
+    6000
+
+    7000
+    8000
+    9000
+
+    10000'
+  end
+
+  it '#calculate should return total food for top elf' do
+    expect(calculate(food)).to eq(24_000)
+  end
+
+  it '#calculate_top_three should return total food for top 3 elves' do
+    expect(calculate_top_three(food)).to eq(45_000)
+  end
+end
