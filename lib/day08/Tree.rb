626af266eb472916c35dcd5dff9c21177feacf7c
Stefan Filev <sfilev@expediagroup.com>
2022-12-08 19:46:58 +0000

    part 02 solution

--- lib/day08/Tree.rb
+++ lib/day08/Tree.rb
@@ -1,9 +1,10 @@
 class Tree
   attr_reader :height
-  attr_accessor :visible
+  attr_accessor :visible, :score
 
   def initialize(height)
     @height = height
     @visible = "????"
+    @score = "?"
   end
 end
--- lib/day08/day08.rb
+++ lib/day08/day08.rb
@@ -14,13 +14,16 @@ def adjust_input(file_path)
   arr
 end
 
-def set_visible_on_outside_trees(arr)
+def set_visible_and_score_on_outside_trees(arr)
   arr.first.each{|tree| tree.visible = true}
+  arr.first.each{|tree| tree.score = 0}
   arr.last.each{|tree| tree.visible = true}
+  arr.last.each{|tree| tree.score = 0}
   arr.each do |small_arr|
     small_arr.each do |tree|
       if small_arr.first == tree || small_arr.last == tree
         tree.visible = true
+        tree.score = 0
       end
     end
   end
@@ -34,7 +37,7 @@ def helper_visualise(arr)
   end
   puts "<------------->"
   arr.each do |small_arr|
-    small_arr.each{|tree| print "#{tree.visible}  "}
+    small_arr.each{|tree| print "#{tree.score}  "}
     puts "\n \n"
   end
 end
@@ -89,8 +92,89 @@ def set_visible_on_inner_trees(arr)
         if left_check == true || right_check == true || up_check == true || down_check == true
           tree.visible = true
         else
-          tree.visible = "fals"
+          tree.visible = false
         end
+      end
+    end
+  end
+end
+
+def set_score_on_inner_trees(arr)
+  arr.each do |row|
+    row.each do |tree|
+      if tree.score == "?"
+        final_score = 0
+
+        left_score = 0
+        right_score = 0
+        up_score = 0
+        down_score = 0
+
+        to_check = row.map {|tr| tr.height}
+        left = to_check[0...row.find_index(tree)]
+        right = to_check[(row.find_index(tree) + 1)..(row.size - 1)]
+        up = []
+        down = []
+        for i in 0..(arr.size) -1
+          for n in 0..(arr[i].size) -1
+              if (n == row.find_index(tree))
+                if (i < arr.find_index(row))
+                  up << arr[i][n].height
+                elsif (i > arr.find_index(row))
+                  down << arr[i][n].height
+                end
+              end
+          end
+        end
+        left_check = left.all? {|value| value < tree.height}
+        if left_check == true
+          left_score += (left.size)
+        else
+          left.reverse_each do |t|
+            left_score += 1
+             if t >= tree.height
+              break
+            end
+          end
+        end
+
+        right_check = right.all? {|value| value < tree.height}
+        if right_check == true
+          right_score += (right.size)
+        else
+          right.each do |t|
+            right_score += 1
+            if t >= tree.height
+             break
+           end
+         end
+        end
+
+        up_check = up.all? {|value| value < tree.height}
+        if up_check == true
+          up_score += (up.size)
+        else
+          up.reverse_each do |t|
+            up_score += 1
+            if t >= tree.height
+             break
+           end
+         end
+        end
+
+        down_check = down.all? {|value| value < tree.height}
+        if down_check == true
+          down_score += (down.size)
+        else
+          down.each do |t|
+            down_score += 1
+            if t >= tree.height
+             break
+           end
+         end
+        end
+        final_score = left_score * right_score * up_score * down_score
+        tree.score = final_score
       end
     end
   end
@@ -108,10 +192,24 @@ def calculate_visible(arr)
   sum
 end
 
-arr = adjust_input('lib/day08/input.csv')
-arr_visib = set_visible_on_outside_trees(arr)
-# p adjust_input('lib/day08/test.csv')
-# helper_visualise(arr_visib)
-set_visible_on_inner_trees(arr_visib)
-# helper_visualise(arr_visib)
-p calculate_visible(arr_visib)
+def find_best_score(arr)
+  best = 0
+  arr.each do |row|
+    row.each do |tree|
+      best = tree.score if tree.score > best
+    end
+  end
+  best
+end
+
+# Part 01
+# arr = adjust_input('lib/day08/test.csv')
+# arr_visib = set_visible_and_score_on_outside_trees(arr)
+# set_visible_on_inner_trees(arr_visib)
+# p calculate_visible(arr_visib)
+
+# Part 02
+# arr = adjust_input('lib/day08/test.csv')
+# arr_visib = set_visible_and_score_on_outside_trees(arr)
+# set_score_on_inner_trees(arr_visib)
+# p find_best_score(arr_visib)
